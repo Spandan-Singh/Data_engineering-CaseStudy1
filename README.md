3e3a8eefe5e02472656e52de71786ecfc8ad983f
Spandan Singh <spandansingh92@gmail.com>
2024-08-15 18:23:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# pyspark_training
+Case study 1
+
+# to setup we need
+1. Open terminal and Navigate to the CaseStudy1 folder
+2. Create environment <python -m venv ./venv>
+3. activate the environment  by typing <.\venv\Scripts\activate> in terminal
+4. Pip install using the requirements.txt file all dependencies <pip install -r requirements.txt>
+
+
+
+# Extras
+1. To deactivate environment type <deactivate> in terminal
+2. to Freeze all dependencies <pip freeze > requirements.txt> 
---
+++ src/main.py
@@ -0,0 +1,51 @@
+from sys import argv
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import year, month, to_date, regexp_replace, sum
+
+# filepath_sales = './dataset/Global Superstore Sales - Global Superstore Sales.csv'
+# filepath_returns = './dataset/Global Superstore Sales - Global Superstore Returns.csv'
+
+def main(filepath_sales, filepath_returns):
+    spark = SparkSession.builder.appName("case_study_1").master("local[*]").getOrCreate()
+    spark.conf.set("spark.sql.parquet.int96RebaseModeInWrite", "CORRECTED")
+    spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
+
+    df_sales :DataFrame = spark.read.option("header", "true").option("inferSchema", "true").csv(filepath_sales)
+    df_sales.show()
+
+    df_returns :DataFrame = spark.read.option("header", "true").option("inferSchema", "true").csv(filepath_returns)
+    df_returns.show()
+    print("Test")
+
+    # TODO: For the total profit and total quantity calculations the returns data should be used to exclude all returns
+    # Filter out the the data matching in retuns for Sale file
+    df_filtered_sales :DataFrame = df_sales.join(df_returns, ["Order ID"], "leftanti")
+
+    # Casting fields
+    df_filtered_sales = df_filtered_sales\
+                        .withColumn("Order Date", to_date(df_filtered_sales["Order Date"], "MM/dd/yyyy"))\
+                        .withColumn("Profit", regexp_replace("Profit", "\\$", ""))\
+                        .withColumn("Profit", regexp_replace("Profit", ",", "").cast("float"))
+
+    # Creating year and month fields                   
+    df_filtered_sales = df_filtered_sales.withColumn("Year", year(df_filtered_sales["Order Date"]))\
+                        .withColumn("Month", month(df_filtered_sales["Order Date"]))
+    
+    # TODO: That contains the aggregate (sum) Profit and Quantity, based on Year, Month, Category, Sub Category
+    # TODO: Year, Month, Category, Sub Category, Total Quantity Sold, Total Profit
+    df_filtered_sales = df_filtered_sales.groupBy(["Year", "Month", "Category", "Sub-Category"])\
+                                        .agg(sum("Profit").alias("Total Profit"),
+                                            sum("Quantity").alias("Total Quantity Sold"))
+    
+    # TODO: This data has to be stored in a partition based on year month. like year=2014/month=11
+    df_filtered_sales.coalesce(1)\
+        .write.partitionBy(["Year", "Month"]).mode("overwrite").format("csv")\
+        .option("header", "true")\
+        .save("./dataset/case_study_report.csv")\
+
+
+# TODO: UNIT TEST
+if __name__ == '__main__':
+    # TODO: Inputs should be sales and return files
+    _, filepath_sales, filepath_returns = argv
+    main(filepath_sales, filepath_returns)  
